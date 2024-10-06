1092a765f123cae726087a8d6b3b8e9c8f815308
andrewpltnv <andrewpltnv@gmail.com>
2024-10-07 01:38:29 +0300

    add dialog aplly for "read full" for testimonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -3,6 +3,8 @@ import AutoplayCarousel from "../Carousel"
 import { CarouselContent, CarouselItem } from "../ui/carousel"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import clsx from "clsx"
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog"
+import { Button } from "../ui/button"
 
 export default function Testimonials({ testimonials }: { testimonials: Testimonial[] }) {
   const content = (
@@ -17,7 +19,7 @@ export default function Testimonials({ testimonials }: { testimonials: Testimoni
 
   return (
     <div className="flex w-full flex-col bg-slate-50">
-      <AutoplayCarousel content={content} delay={3000} />
+      <AutoplayCarousel content={content} delay={30000} />
     </div>
   )
 }
@@ -35,9 +37,34 @@ const Testimonial = ({ testimonial, isOpen }: { testimonial: Testimonial; isOpen
       </CardHeader>
       <CardContent>
         <p className={clsx({ "line-clamp-[8]": !isOpen }, "select-none hyphens-auto")}>{testimonial.text}</p>
+        {testimonial.text.length > 290 && (
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={"link"}>Читати повністю {">>>"}</Button>
+            </DialogTrigger>
+            <DialogContent className="max-h-screen overflow-scroll p-0">
+              <Card className="flex flex-col">
+                <CardHeader className="pb-2">
+                  <CardTitle>
+                    {testimonial.name}
+                    <CardDescription>
+                      <span>{testimonial.date}</span>
+                    </CardDescription>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className={clsx("hyphens-auto")}>{testimonial.text}</p>
+                </CardContent>
+                <CardFooter className="prose bottom-0 text-xs">
+                  <blockquote className="line-clamp-3 leading-none">{testimonial.source}</blockquote>
+                </CardFooter>
+              </Card>
+            </DialogContent>
+          </Dialog>
+        )}
       </CardContent>
       <CardFooter className="prose absolute bottom-0 text-xs">
-        <blockquote className="line-clamp-3">{testimonial.source}</blockquote>
+        <blockquote className="line-clamp-3 leading-none">{testimonial.source}</blockquote>
       </CardFooter>
     </Card>
   )
