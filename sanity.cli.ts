d640b2ea75e65f7e292ef49a889543ed6c08de14
Andrey Platonov <20723543+andrewpltnv@users.noreply.github.com>
2024-10-23 07:17:39 +0300

    rest of build (#2)

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -9,4 +9,4 @@ import { defineCliConfig } from "sanity/cli"
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "lu5enl2l"
 const dataset = process.env.SANITY_STUDIO_API_DATASET || "production"
 
-export default defineCliConfig({ api: { projectId, dataset }, studioHost: "surgery-lu5enl2l" })
+export default defineCliConfig({ api: { projectId, dataset } })
